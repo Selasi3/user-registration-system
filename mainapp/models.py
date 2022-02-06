00b802df594d08fbbb412e791c890fd1aa960a54
Selasi3 <selasiayittah3@gmail.com>
2022-02-06 09:28:11 -1200

    initial commit

---
+++ mainapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.core.validators import RegexValidator
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    other_names = models.CharField(max_length=100, null=True, blank=True)
+    phoneNumberRegex = RegexValidator(regex = r"^\+?1?\d{8,15}$")
+    number = models.CharField(validators=[phoneNumberRegex], max_length=17, blank=True)
+    email = models.EmailField(max_length=100)
+    time_registered = models.DateTimeField(default= timezone.now)
+    Gender =(("M","Male" ),("F","Female"))
+    gender = models.CharField(max_length=1, choices=Gender, blank=False)
+
+
+    def __str__(self):
+        return f"{self.first_name} , {self.last_name}"
