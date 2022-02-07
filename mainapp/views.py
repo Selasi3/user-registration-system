f33eed2f24b179704fbc2f788aa8795814db2c37
Selasi Ayittah <selasiayittah3@gmail.com>
2022-02-07 17:24:23 +0000

    implent functionality to display  the index.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Person
 # Create your views here.
+
+def index(request):
+    totalnumber = Person.objects.all().count();
+    context = {
+        "totalnumber":totalnumber
+    }
+    return render(request, 'index.html',context=context)
+
